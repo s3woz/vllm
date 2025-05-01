a6cc0f51b4c1cc47e1b04921eb0dde62fa772d84
Thomas Ortner <boh@zurich.ibm.com>
2025-05-01 16:58:58 +0000

    Integrated code review; Moved tests; Added model to test_hybrid.py
    Signed-off-by: Thomas Ortner <boh@zurich.ibm.com>

--- tests/models/language/generation/test_hybrid.py
+++ tests/models/language/generation/test_hybrid.py
@@ -23,6 +23,9 @@ SSM_MODELS = [
 
 HYBRID_MODELS = [
     "ai21labs/Jamba-tiny-dev",
+    # NOTE: ibm-research/granite-4.0-tiny-test are skipped currently as
+    # the HF model URLs not available yet
+    "ibm-research/granite-4.0-tiny-test",
     # NOTE: Running Plamo2 in transformers implementation requires to install
     # causal-conv1d package, which is not listed as a test dependency as it's
     # not compatible with pip-compile.
@@ -46,6 +49,9 @@ def test_models(
     max_tokens: int,
     num_logprobs: int,
 ) -> None:
+    if model == "ibm-research/granite-4.0-tiny-test":
+        pytest.skip(reason="HF model URLs not available yet")
+    
     with hf_runner(model) as hf_model:
         hf_outputs = hf_model.generate_greedy_logprobs_limit(
             example_prompts, max_tokens, num_logprobs)
@@ -72,6 +78,9 @@ def test_batching(
     max_tokens: int,
     num_logprobs: int,
 ) -> None:
+    if model == "ibm-research/granite-4.0-tiny-test":
+        pytest.skip(reason="HF model URLs not available yet")
+    
     for_loop_outputs = []
     with vllm_runner(model, max_num_seqs=MAX_NUM_SEQS) as vllm_model:
         for prompt in example_prompts:
@@ -91,7 +100,7 @@ def test_batching(
     )
 
 
-@pytest.mark.parametrize("model", [SSM_MODELS[0], HYBRID_MODELS[0]])
+@pytest.mark.parametrize("model", SSM_MODELS[0:1] + HYBRID_MODELS[0:2])
 @pytest.mark.parametrize("max_tokens", [32])
 @pytest.mark.parametrize("num_logprobs", [5])
 @pytest.mark.parametrize("chunked_prefill_token_size", [1, 4, 16])
@@ -103,6 +112,9 @@ def test_chunked_prefill(
     num_logprobs: int,
     chunked_prefill_token_size: int,
 ) -> None:
+    if model == "ibm-research/granite-4.0-tiny-test":
+        pytest.skip(reason="HF model URLs not available yet")
+        
     max_num_seqs = chunked_prefill_token_size
     max_num_batched_tokens = chunked_prefill_token_size
 
@@ -127,7 +139,7 @@ def test_chunked_prefill(
     )
 
 
-@pytest.mark.parametrize("model", [SSM_MODELS[0], HYBRID_MODELS[0]])
+@pytest.mark.parametrize("model", SSM_MODELS[0:1] + HYBRID_MODELS[0:2])
 @pytest.mark.parametrize("max_tokens", [10])
 def test_chunked_prefill_with_parallel_sampling(
     vllm_runner,
@@ -145,6 +157,9 @@ def test_chunked_prefill_with_parallel_sampling(
     decoding steps inside a chunked prefill forward pass
     (where we have both prefill and decode together)
     """
+    if model == "ibm-research/granite-4.0-tiny-test":
+        pytest.skip(reason="HF model URLs not available yet")
+    
     sampling_params = SamplingParams(n=3,
                                      temperature=1,
                                      seed=0,
@@ -159,7 +174,7 @@ def test_chunked_prefill_with_parallel_sampling(
         vllm_model.generate(example_prompts, sampling_params)
 
 
-@pytest.mark.parametrize("model", [SSM_MODELS[0], HYBRID_MODELS[0]])
+@pytest.mark.parametrize("model", SSM_MODELS[0:1] + HYBRID_MODELS[0:2])
 @pytest.mark.parametrize("max_tokens", [20])
 def test_mamba_cache_cg_padding(
     vllm_runner,
@@ -172,6 +187,9 @@ def test_mamba_cache_cg_padding(
     batch size. If it's not, a torch RuntimeError will be raised because
     tensor dimensions aren't compatible.
     """
+    if model == "ibm-research/granite-4.0-tiny-test":
+        pytest.skip(reason="HF model URLs not available yet")
+    
     vllm_config = EngineArgs(model=model,
                              trust_remote_code=True).create_engine_config()
     while len(example_prompts) == vllm_config.pad_for_cudagraph(
@@ -188,7 +206,7 @@ def test_mamba_cache_cg_padding(
             "Could be related to mamba cache not padded correctly")
 
 
-@pytest.mark.parametrize("model", [SSM_MODELS[0], HYBRID_MODELS[0]])
+@pytest.mark.parametrize("model", SSM_MODELS[0:1] + HYBRID_MODELS[0:2])
 @pytest.mark.parametrize("max_tokens", [20])
 def test_models_preemption_recompute(
     vllm_runner,
@@ -199,6 +217,9 @@ def test_models_preemption_recompute(
     """
     Tests that outputs are identical with and w/o preemptions (recompute).
     """
+    if model == "ibm-research/granite-4.0-tiny-test":
+        pytest.skip(reason="HF model URLs not available yet")
+    
     with vllm_runner(model, max_num_seqs=MAX_NUM_SEQS) as vllm_model:
         scheduler = vllm_model.model.llm_engine.scheduler[0]
         scheduler.ENABLE_ARTIFICIAL_PREEMPT = True
@@ -216,7 +237,7 @@ def test_models_preemption_recompute(
     )
 
 
-@pytest.mark.parametrize("model", [SSM_MODELS[0], HYBRID_MODELS[0]])
+@pytest.mark.parametrize("model", SSM_MODELS[0:1] + HYBRID_MODELS[0:2])
 def test_fail_upon_inc_requests_and_finished_requests_lt_available_blocks(
     vllm_runner,
     example_prompts,
@@ -231,6 +252,9 @@ def test_fail_upon_inc_requests_and_finished_requests_lt_available_blocks(
     statelessness mechanism where it can cleanup new incoming requests in
     a single step.
     """
+    if model == "ibm-research/granite-4.0-tiny-test":
+        pytest.skip(reason="HF model URLs not available yet")
+    
     try:
         with vllm_runner(model, max_num_seqs=MAX_NUM_SEQS) as vllm_model:
             vllm_model.generate_greedy([example_prompts[0]] * 100, 10)
@@ -239,7 +263,7 @@ def test_fail_upon_inc_requests_and_finished_requests_lt_available_blocks(
                     "steps finished requests registered unnecessarily ")
 
 
-@pytest.mark.parametrize("model", [SSM_MODELS[0], HYBRID_MODELS[0]])
+@pytest.mark.parametrize("model", SSM_MODELS[0:1] + HYBRID_MODELS[0:2])
 def test_state_cleanup(
     vllm_runner,
     example_prompts,
@@ -251,6 +275,9 @@ def test_state_cleanup(
     
     If its not cleaned, an error would be expected.
     """
+    if model == "ibm-research/granite-4.0-tiny-test":
+        pytest.skip(reason="HF model URLs not available yet")
+    
     try:
         with vllm_runner(model, max_num_seqs=MAX_NUM_SEQS) as vllm_model:
             for _ in range(10):
@@ -260,7 +287,7 @@ def test_state_cleanup(
                     "could be related to finished_requests_ids")
 
 
-@pytest.mark.parametrize("model", [SSM_MODELS[0], HYBRID_MODELS[0]])
+@pytest.mark.parametrize("model", SSM_MODELS[0:1] + HYBRID_MODELS[0:2])
 @pytest.mark.parametrize("max_tokens", [64])
 def test_multistep_correctness(
     vllm_runner,
@@ -268,6 +295,9 @@ def test_multistep_correctness(
     model: str,
     max_tokens: int,
 ) -> None:
+    if model == "ibm-research/granite-4.0-tiny-test":
+        pytest.skip(reason="HF model URLs not available yet")
+    
     with vllm_runner(model, num_scheduler_steps=8,
                      max_num_seqs=2) as vllm_model:
         vllm_outputs_multistep = vllm_model.generate_greedy(
